8103ddaac8e60fc0681679d91f7afc1a7cc61904
pelon <peloycabazos@gmail.com>
2026-03-25 01:07:53 -0400

    update firebase file

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Importar desde CDN (IMPORTANTE)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWHLkvYjVGvdA-QLh7Nmu9PiNYb8h3rZU",
+  authDomain: "movie-tracker-67399.firebaseapp.com",
+  projectId: "movie-tracker-67399",
+  storageBucket: "movie-tracker-67399.firebasestorage.app",
+  messagingSenderId: "537030759923",
+  appId: "1:537030759923:web:8282164545bb3ccc082e86"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
